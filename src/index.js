aeb2f9438fe7749c0e26cf021b31d38074f4087b
zombym <zombymaster@gmail.com>
2018-02-12 14:16:19 +0300

    add index.js

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import readlineSync from 'readline-sync';
+console.log('Welcome to the brain Games!');
+const actual = readlineSync.question('Your answer: ');
+
+
+
+console.log('Hi ' + actual + '!');
