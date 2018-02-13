c714393a57f5ae028152e2224c70d4ee733507b0
zombym <zombymaster@gmail.com>
2018-02-13 17:49:10 +0300

    refactoring

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,19 +1,4 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
+import evengame from '../index';
 
-console.log('Welcome to the brain Games! \nAnswer "yes" if number even otherwise answer "no".\n');
-const name = readlineSync.question('May I have your name? ');
-console.log(`Hello ${name}!`);
-let count = 0;
-for (let i = 0; i < 3; i += 1) {
-  const a = Math.floor(Math.random() * 100);
-  let b = '';
-  console.log(`Question: ${a}!`);
-  const answer = readlineSync.question('Your answer: ');
-  (a % 2 === 0) ? b = 'yes' : b = 'no';
-  if (answer === b) {
-    console.log('Correct!');
-    count += 1;
-  } else console.log(`'${answer}' is wrong answer ;(. Correct answer was '${b}'\nLet's try again, ${name}`);
-}
-if (count === 3) console.log(`Congratulations, ${name}`);
+evengame();
--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
 
-console.log('Welcome to the brain Games!');
-const name = readlineSync.question('Your answer: ');
-console.log(`Hi ${name} !!!`);
+const evengame = () => {
+  console.log('Welcome to the brain Games! \nAnswer "yes" if number even otherwise answer "no".\n');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello ${name}!`);
+  let count = 0;
+  for (let i = 0; i < 3; i += 1) {
+    const a = Math.floor(Math.random() * 100);
+    let b = '';
+    console.log(`Question: ${a}!`);
+    const answer = readlineSync.question('Your answer: ');
+    (a % 2 === 0) ? b = 'yes' : b = 'no';
+    if (answer === b) {
+      console.log('Correct!');
+      count += 1;
+    } else console.log(`'${answer}' is wrong answer ;(. Correct answer was '${b}'\nLet's try again, ${name}`);
+  }
+  if (count === 3) console.log(`Congratulations, ${name}`);
+};
+
+export default evengame;
