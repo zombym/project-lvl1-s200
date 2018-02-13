dae9cd49f4d6fa1302eb34f0d7aa198b72c03bac
zombym <zombymaster@gmail.com>
2018-02-13 16:39:30 +0300

    lvl1t4

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,2 +1,19 @@
 #!/usr/bin/env node
-console.log('Welcome to the brain Games!');
+import readlineSync from 'readline-sync';
+
+console.log('Welcome to the brain Games! \nAnswer "yes" if number even otherwise answer "no".\n');
+const name = readlineSync.question('May I have your name? ');
+console.log(`Hello ${name}!`);
+let count = 0;
+for (let i = 0; i < 3; i += 1) {
+  const a = Math.floor(Math.random() * 100);
+  let b = '';
+  console.log(`Question: ${a}!`);
+  const answer = readlineSync.question('Your answer: ');
+  (a % 2 === 0) ? b = 'yes' : b = 'no';
+  if (answer === b) {
+    console.log('Correct!');
+    count += 1;
+  } else console.log(`'${answer}' is wrong answer ;(. Correct answer was '${b}'\nLet's try again, ${name}`);
+}
+if (count === 3) console.log(`Congratulations, ${name}`);
