1eed7d6421a27394e49bc825c9cb2b6ff490de8e
zombym <zombymaster@gmail.com>
2018-02-17 00:03:50 +0300

    lvl1t9 version 0.0.39

---
+++ src/bin/brain-simple.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+import simple from '../games/simple';
+
+simple();
---
+++ src/games/simple.js
@@ -0,0 +1,22 @@
+import { engine, random } from '../index';
+
+const task = '';
+
+const rezSimple = () => {
+  const a = random(0, 100);
+  let count = 0;
+  const simpleCheck = (num) => {
+    for (let i = 1; i <= num; i += 1) {
+      if (num % i === 0) count += 1;
+    }
+    if (count === 2) return true;
+    return false;
+  };
+  const rez = (simpleCheck(a)) ? 'yes' : 'no';
+  const txt = `Is this number prime?: ${a}!`;
+  return { txt, rez };
+};
+
+const simple = () => engine(task, rezSimple);
+
+export default simple;
